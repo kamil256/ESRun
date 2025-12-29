87574e2f4c5ff422a26c2fa3175c403ca466c9cc
Kamil <kamil.dzierlega@one-solution.com>
2025-12-29 10:25:12 +0100

    feat: Implement StringValue class for string representation and manipulation

---
+++ ESRun.Interpreter/EsTypes/String/String.cs
@@ -0,0 +1,66 @@
+using ESRun.Interpreter.EsTypes.Abstract;
+
+namespace ESRun.Interpreter.EsTypes.String;
+
+public class StringValue : EsValue
+{
+    public const string NaNLiteral = "NaN";
+    public const string InfinityLiteral = "Infinity";
+    public const string NegativeInfinityLiteral = "-Infinity";
+
+    private readonly string _value;
+
+    public StringValue(string literal)
+    {
+        _value = literal;
+    }
+
+    public override string ToString()
+    {
+        return _value;
+    }
+
+    public override StringValue Clone()
+    {
+        return new StringValue(ToString());
+    }
+
+    public override string ToString(int nestingLevel)
+    {
+        return ToString();
+    }
+
+    public string GetSubstring(int inclusiveStartIndex, int exclusiveEndIndex)
+    {
+        return _value.Substring(inclusiveStartIndex, exclusiveEndIndex - inclusiveStartIndex);
+    }
+
+    public int StringIndexOf(string searchValue, int fromIndex)
+    {
+        var len = _value.Length;
+
+        if (searchValue.Length == 0 && fromIndex <= len)
+        {
+            return fromIndex;
+        }
+
+        var searchLen = searchValue.Length;
+
+        for (var i = fromIndex; i <= len - searchLen; i++)
+        {
+            var candiate = GetSubstring(i, i + searchLen);
+
+            if (candiate == searchValue)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    public int StringLastIndexOf(string searchValue, int fromIndex)
+    {
+        throw new NotImplementedException();
+    }
+}
